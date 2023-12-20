c1675b48b742b3705a27c11ab05fdf6ee9f541b0
Facundo_Veron <vfacundo2010@gmail.com>
2023-12-20 11:15:07 -0300

    mi primer commit

---
+++ src/app.js
@@ -0,0 +1,55 @@
+const ProductManager = require("./productManager")
+
+const express = require("express");
+
+const manager = new ProductManager("productos.json")
+
+const app = express();
+
+const PORT = 9090;
+
+app.get('/products', async (req, res) => {
+    try {
+        const arrayProductos = await manager.leerArchivo();
+
+        let limit = parseInt(req.query.limit)
+
+        if (limit) {
+            const arrayConLimit = productos.slice(0, limit)
+            return res.send(arrayConLimit);
+        } else {
+            return res.send(arrayProductos);
+        }
+
+    } catch (error) {
+        console.log(error)
+        return res.send("error al procesar la solicitud")
+    }
+})
+
+app.get('/products/:pid', async (req, res) => {
+    try {
+        //Usamos query params para tomar el valor de id asignado x url
+        let productId = parseInt(req.params.pid)
+
+        const product = await manager.getProductById(productId)
+
+        if (product) {
+            res.send(product)
+        } else {
+            res.status(404).json({ error: 'producto no encontrado' })
+        }
+
+    } catch (error) {
+        res.status(501).json({ error: 'Producto con id no encontrado' })
+    }
+})
+
+//iniciamos server
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`)
+})
+
+
+
+
