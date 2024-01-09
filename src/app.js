34b244886607ea640ad4750d5cef6c12d32a99ab
Facundo_Veron <vfacundo2010@gmail.com>
2024-01-09 17:19:58 -0300

    1 commit

--- src/app.js
+++ src/app.js
@@ -1,54 +1,20 @@
-const ProductManager = require("./productManager.js");
-
 const express = require("express");
-
-const manager = new ProductManager("./productos.json")
 
 const app = express();
 
 const PORT = 8080;
 
+const productsRouter = require("./routes/products.router.js");
+
+const cartsRouter = require("./routes/carts.router.js");
+
 app.use(express.urlencoded({extended : true}))
 
 app.use(express.json());
 
-app.get('/products', async (req, res) => {
-    try {
-        const arrayProductos = await manager.getProducts();
+app.use("/api", productsRouter);
 
-        let limit = parseInt(req.query.limit)
-
-        if (limit) {
-            const arrayConLimit = arrayProductos.slice(0, limit)
-            
-            return res.send(arrayConLimit);
-        } else {
-            return res.send(arrayProductos);
-        }
-
-    } catch (error) {
-        console.log(error)
-        return res.send("error al procesar la solicitud")
-    }
-})
-
-app.get('/products/:pid', async (req, res) => {
-    try {
-        //Usamos query params para tomar el valor de id asignado x url
-        let productId = parseInt(req.params.pid)
-
-        const product = await manager.getProductById(productId)
-
-        if (product) {
-            res.send(product)
-        } else {
-            res.status(404).json({ error: 'producto no encontrado' })
-        }
-
-    } catch (error) {
-        res.status(501).json({ error: 'Producto con id no encontrado' })
-    }
-})
+app.use("/api", cartsRouter);
 
 //iniciamos server
 app.listen(PORT, () => {
